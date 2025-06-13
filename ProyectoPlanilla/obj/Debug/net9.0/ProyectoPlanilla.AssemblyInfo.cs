708ae8d1ffcf3050295b96c37c7d8cf475473993
Quirkio <jose.ignacio.madriz@gmail.com>
2025-06-12 21:12:14 -0600

    Fuck me

--- ProyectoPlanilla/obj/Debug/net9.0/ProyectoPlanilla.AssemblyInfo.cs
+++ ProyectoPlanilla/obj/Debug/net9.0/ProyectoPlanilla.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoPlanilla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8228bd2cec59222fcc89baf6c67c989df208ae0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f0d33ca91d6a603fd3c45760780542c62ec30c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoPlanilla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoPlanilla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
